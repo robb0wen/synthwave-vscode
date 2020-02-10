309b61141beb4764308c62fc5058f15787d2b04b
Robb Owen <hello@robbowen.digital>
2020-02-10 12:43:41 +0000

    Show what's new page on each minor version.

--- README.md
+++ README.md
@@ -19,25 +19,39 @@ Much the same way, in the modern web-development world of shaders, React and Web
 ## Installation
 To begin with, [install the base theme from the VS Marketplace](https://marketplace.visualstudio.com/items?itemName=RobbOwen.synthwave-vscode). This is the way Synthwave '84 is intended to be used day-to-day. If you want to enable the gratuitous 80s glow, it needs a little extra work to get it going. 
 
-VS code doesn't natively support text effects and as a result, the glow is experimental. It's likely to be buggy and, whilst it looks rad, it isn't intended for extended use. Enabling the glow requires that the extension modifies the internal electron-app of VS Code, so use with caution.
+### Disclaimer
+VS code doesn't natively support text effects and as a result, the glow is experimental. It's likely to be buggy and, whilst it looks rad, it isn't intended for extended use. To enable the glow, the extension has to modify the internal files of VS Code, so use with caution. Should something go wrong, you can disable the glow by following the instructions below. If for any reason you can't open VS Code, you can fix the issue with a fresh install of VS Code.
 
 If you do decide use the glow effect, you do so at your own risk. Bring your Sunglasses. Here be (laser)dragons.
 
 ### To enable the glow
-As of v0.1.0, external extensions are no longer needed to active the glow. Open your command palette with `Ctrl + Shift + P` or `Shift + ⌘ + P` and choose "__Enable Neon Dreams__". It will prompt you to restart, and when you do the lights should be on :)
+As of v0.1.0, external extensions are no longer needed to active the glow. 
+
+Firstly, if you are a Windows user, you may need to run VS Code with administrator priviliges. For Linux and Mac users, Code must not be installed in a read-only location and you must have write permissions.
+
+If you have been using Synthwave prior to v0.1.0, you will likely have used the Custom CSS and JS extension to enable the glow. Before enabling Neon Dreams, you will need to disable the prior method. You can do this by pressing `Ctrl + Shift + P` or `Shift + ⌘ + P` and choose "__Disable Custom CSS and JS__"
+
+To activate the glow, Set your active colour theme to Synthwave '84 - as of v0.1.0 the glow is only active when the base theme is selected. Open your command palette with `Ctrl + Shift + P` or `Shift + ⌘ + P` and choose "__Enable Neon Dreams__". It will prompt you to restart, and when you do the lights should be on :)
+
+Note: In the name of simplifying the install process and mitigating update-related issues, the new version of Synthwave '84 doesn't use a custom CSS file anymore. _If you have been using a custom-modified version of the theme, then you can still continue to use that with the previous Custom CSS and JS method_.
 
 #### To customise the glow brightness
 In your `settings.json` add the key:
 ```
 "synthwave84.brightness": "0.45"
 ```
-The value should be a _float value_ from 0 to 1, where 00 is fully transparent. The default brightness is 0.45. To avoid eye strain, avoid using higher brightness values for extended periods of time.
+The value should be a _float value_ from 0 to 1, where 0.0 is fully transparent. The default brightness is 0.45. To avoid eye strain, avoid using higher brightness values for extended periods of time. 
+
+To see the changes, you need to rerun the activation function. Open your command palette with `Ctrl + Shift + P` or `Shift + ⌘ + P` and choose "__Enable Neon Dreams__".
+
+Note: Changing the brightness currently only affects the opacity of the glow, the text will remain white (that may change in future updates). If you want to disable the glow effect but retain the chrome updates, see below.
 
 #### To enable editor chrome updates, but disable glow
 In your `settings.json` add the key:
 ```
 "synthwave84.disableGlow": true
 ```
+To see the changes, you need to rerun the activation function. Open your command palette with `Ctrl + Shift + P` or `Shift + ⌘ + P` and choose "__Enable Neon Dreams__".
 
 ## Updates
 Every time you update VS code, you will need to repeat this step to re-enable the glow.
@@ -49,7 +63,7 @@ The glow effect started as a joke and was never intended for long-term coding se
 I haven't included a font in this release as I know that it's a very personal preference. The font I use (that is seen in the image above) is [Fira Code](https://github.com/tonsky/FiraCode), which I recommend if you're a fan of ligatures.
 
 ## Compatibility
-This theme is **very much a work in progress**. I primarily develop in HTML & CSS, JS, React and Elixir so, whilst those language sets should look pretty good, there will likely be issues for other languages. I'll work on adding more support as I go. If you find anything glaringly wrong, raise an issue and I'll try to fix it as soon as I can.
+This theme is still **very much a work in progress**. I primarily develop in HTML & CSS, JS, React and Elixir so, whilst those language sets should look pretty good, there will likely be issues for other languages. I'll work on adding more support as I go. If you find anything glaringly wrong, raise an issue and I'll try to fix it as soon as I can.
 
 ## Contributing
 I'm really happy to consider any contributions to this theme. Before you make any changes, [please read the contribution guide](https://github.com/robb0wen/synthwave-vscode/blob/master/CONTRIBUTING.md).
--- src/extension.js
+++ src/extension.js
@@ -1,11 +1,37 @@
 const path = require('path');
 const fs = require('fs');
 const vscode = require('vscode');
+const diff = require('semver/functions/diff');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	this.extensionName = 'RobbOwen.synthwave-vscode';
+	this.cntx = context;
+	this.extension = vscode.extensions.getExtension(this.extensionName);
+	if (this.extension) {
+		// grab current version number
+		this.version = this.extension.packageJSON.version;
+
+		// grab last recorded version
+		const prevVersion = context.globalState.get(`${this.extensionName}.version`);
+
+		if (prevVersion) {
+			// check it has changed.
+			const d = diff(this.version, prevVersion);
+			// show again on major or minor updates
+			if (d == 'major' || d == 'minor') {
+				showUpdatePage();
+				context.globalState.update(`${this.extensionName}.version`, this.version);
+			}
+		} else {
+			showUpdatePage();
+			context.globalState.update(`${this.extensionName}.version`, this.version);
+		}
+
+	}
+	
 	const config = vscode.workspace.getConfiguration("synthwave84");
 
 	let disableGlow = config && config.disableGlow ? !!config.disableGlow : false;
@@ -17,12 +43,11 @@ function activate(context) {
 	const parsedBrightness = Math.floor(brightness * 255).toString(16).toUpperCase();
 	let neonBrightness = parsedBrightness;
 
-	let disposable = vscode.commands.registerCommand('extension.enableNeon', function () {
+	let disposable = vscode.commands.registerCommand('synthwave84.enableNeon', function () {
+
 		const isWin = /^win/.test(process.platform);
 		const appDir = path.dirname(require.main.filename);
-
 		const base = appDir + (isWin ? "\\vs\\code" : "/vs/code");
-		console.log(base);
 
 		const htmlFile =
 			base +
@@ -37,6 +62,9 @@ function activate(context) {
 					: "/electron-browser/workbench/neondreams.js");
 
 		try {
+
+			// const version = context.globalState.get(`${context.extensionName}.version`);
+
 			// generate production theme JS
 			const chromeStyles = fs.readFileSync(__dirname +'/css/editor_chrome.css', 'utf-8');
 			const jsTemplate = fs.readFileSync(__dirname +'/js/theme_template.js', 'utf-8');
@@ -59,25 +87,58 @@ function activate(context) {
 				output += '</html>';
 	
 				fs.writeFileSync(htmlFile, output, "utf-8");
-				vscode.commands.executeCommand("workbench.action.reloadWindow");
+				
+				vscode.window
+					.showInformationMessage("Neon Dreams enabled. VS code must reload for this change to take effect. Code may display a warning that it is corrupted, this is normal. You can dismis this message by choosing 'Don't show this again' on the notification.", { title: "Restart editor to complete" })
+					.then(function(msg) {
+						vscode.commands.executeCommand("workbench.action.reloadWindow");
+					});
+
 			} else {
-				vscode.window.showInformationMessage('Neon dreams is already enabled. Refreshing JS settings.');
+				vscode.window
+					.showInformationMessage('Neon dreams is already enabled. Reload to refresh JS settings.', { title: "Restart editor to refresh settings" })
+					.then(function(msg) {
+						vscode.commands.executeCommand("workbench.action.reloadWindow");
+					});
 			}
 		} catch (e) {
-			vscode.window.showErrorMessage('Something went wrong when starting neon dreams');
+			if (/ENOENT|EACCES|EPERM/.test(e.code)) {
+				vscode.window.showInformationMessage("You must run VS code with admin priviliges in order to enable Neon Dreams.");
+				return;
+			} else {
+				vscode.window.showErrorMessage('Something went wrong when starting neon dreams');
+				return;
+			}
 		}
 	});
 
-	let disable = vscode.commands.registerCommand('extension.disableNeon', uninstall);
-
+	let disable = vscode.commands.registerCommand('synthwave84.disableNeon', uninstall);
+	let whatsNew = vscode.commands.registerCommand('synthwave84.whatsNew', showUpdatePage);
+	
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(disable);
+	context.subscriptions.push(whatsNew);
 }
 exports.activate = activate;
 
+
+function showUpdatePage() {
+		const panel = vscode.window.createWebviewPanel(
+			`synthwave.whatsNew`, // Identifies the type of the webview. Used internally
+			'What\'s new for Synthwave \'84', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{ enableScripts: !0 } // Webview options. More on these later.
+		);
+
+		const viewPath = path.join(this.cntx.extensionPath, "whats-new", "view.html");
+		const viewResourcePath = panel.webview.asWebviewUri(viewPath);
+		const htmlContent = fs.readFileSync(viewPath, "utf-8");
+		panel.webview.html = htmlContent;
+}
+
 // this method is called when your extension is deactivated
 function deactivate() {
-	// uninstall();
+	// ...
 }
 
 function uninstall() {
@@ -100,7 +161,12 @@ function uninstall() {
 		// delete synthwave script tag if there
 		let output = html.replace(/^.*(<!-- SYNTHWAVE 84 --><script src="neondreams.js"><\/script><!-- NEON DREAMS -->).*\n?/mg, '');
 		fs.writeFileSync(htmlFile, output, "utf-8");
-		vscode.commands.executeCommand("workbench.action.reloadWindow");
+
+		vscode.window
+			.showInformationMessage("Neon Dreams disabled. VS code must reload for this change to take effect", { title: "Restart editor to complete" })
+			.then(function(msg) {
+				vscode.commands.executeCommand("workbench.action.reloadWindow");
+			});
 	} else {
 		vscode.window.showInformationMessage('Neon dreams isn\'t running.');
 	}

8144fdb418bb08ee006aa52490ecef39dcc838b3
Anton Kosyakov <anton@gitpod.io>
2021-07-27 04:19:09 +0000

    [remote] sync version across different machines
    to avoid showing the same update page to the same user on different machines over and over again

--- src/extension.js
+++ src/extension.js
@@ -10,12 +10,16 @@ function activate(context) {
 	this.extensionName = 'RobbOwen.synthwave-vscode';
 	this.cntx = context;
 	this.extension = vscode.extensions.getExtension(this.extensionName);
+
+	const versionKey = `${this.extensionName}.version`;
+	context.globalState.setKeysForSync([versionKey]);
+
 	if (this.extension) {
 		// grab current version number
 		this.version = this.extension.packageJSON.version;
 
 		// grab last recorded version
-		const prevVersion = context.globalState.get(`${this.extensionName}.version`);
+		const prevVersion = context.globalState.get(versionKey);
 
 		if (prevVersion) {
 			// check it has changed.
@@ -23,19 +27,19 @@ function activate(context) {
 			// show again on major or minor updates
 			if (d == 'major' || d == 'minor') {
 				showUpdatePage();
-				context.globalState.update(`${this.extensionName}.version`, this.version);
+				context.globalState.update(versionKey, this.version);
 			}
 		} else {
 			showUpdatePage();
-			context.globalState.update(`${this.extensionName}.version`, this.version);
+			context.globalState.update(versionKey, this.version);
 		}
 
 	}
-	
+
 	const config = vscode.workspace.getConfiguration("synthwave84");
 
 	let disableGlow = config && config.disableGlow ? !!config.disableGlow : false;
-	
+
 	let brightness = parseFloat(config.brightness) > 1 ? 1 : parseFloat(config.brightness);
 	brightness = brightness < 0 ? 0 : brightness;
 	brightness = isNaN(brightness) ? 0.45 : brightness;
@@ -56,23 +60,23 @@ function activate(context) {
 				: "/electron-browser/workbench/workbench.html");
 
 		const templateFile =
-				base +
-				(isWin
-					? "\\electron-browser\\workbench\\neondreams.js"
-					: "/electron-browser/workbench/neondreams.js");
+			base +
+			(isWin
+				? "\\electron-browser\\workbench\\neondreams.js"
+				: "/electron-browser/workbench/neondreams.js");
 
 		try {
 
 			// const version = context.globalState.get(`${context.extensionName}.version`);
 
 			// generate production theme JS
-			const chromeStyles = fs.readFileSync(__dirname +'/css/editor_chrome.css', 'utf-8');
-			const jsTemplate = fs.readFileSync(__dirname +'/js/theme_template.js', 'utf-8');
+			const chromeStyles = fs.readFileSync(__dirname + '/css/editor_chrome.css', 'utf-8');
+			const jsTemplate = fs.readFileSync(__dirname + '/js/theme_template.js', 'utf-8');
 			const themeWithGlow = jsTemplate.replace(/\[DISABLE_GLOW\]/g, disableGlow);
 			const themeWithChrome = themeWithGlow.replace(/\[CHROME_STYLES\]/g, chromeStyles);
 			const finalTheme = themeWithChrome.replace(/\[NEON_BRIGHTNESS\]/g, neonBrightness);
 			fs.writeFileSync(templateFile, finalTheme, "utf-8");
-			
+
 			// modify workbench html
 			const html = fs.readFileSync(htmlFile, "utf-8");
 
@@ -85,19 +89,19 @@ function activate(context) {
 				// add script tag
 				output = html.replace(/\<\/html\>/g, `	<!-- SYNTHWAVE 84 --><script src="neondreams.js"></script><!-- NEON DREAMS -->\n`);
 				output += '</html>';
-	
+
 				fs.writeFileSync(htmlFile, output, "utf-8");
-				
+
 				vscode.window
 					.showInformationMessage("Neon Dreams enabled. VS code must reload for this change to take effect. Code may display a warning that it is corrupted, this is normal. You can dismiss this message by choosing 'Don't show this again' on the notification.", { title: "Restart editor to complete" })
-					.then(function(msg) {
+					.then(function (msg) {
 						vscode.commands.executeCommand("workbench.action.reloadWindow");
 					});
 
 			} else {
 				vscode.window
 					.showInformationMessage('Neon dreams is already enabled. Reload to refresh JS settings.', { title: "Restart editor to refresh settings" })
-					.then(function(msg) {
+					.then(function (msg) {
 						vscode.commands.executeCommand("workbench.action.reloadWindow");
 					});
 			}
@@ -114,7 +118,7 @@ function activate(context) {
 
 	let disable = vscode.commands.registerCommand('synthwave84.disableNeon', uninstall);
 	let whatsNew = vscode.commands.registerCommand('synthwave84.whatsNew', showUpdatePage);
-	
+
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(disable);
 	context.subscriptions.push(whatsNew);
@@ -123,17 +127,17 @@ exports.activate = activate;
 
 
 function showUpdatePage() {
-		const panel = vscode.window.createWebviewPanel(
-			`synthwave.whatsNew`, // Identifies the type of the webview. Used internally
-			'What\'s new for Synthwave \'84', // Title of the panel displayed to the user
-			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-			{ enableScripts: !0 } // Webview options. More on these later.
-		);
+	const panel = vscode.window.createWebviewPanel(
+		`synthwave.whatsNew`, // Identifies the type of the webview. Used internally
+		'What\'s new for Synthwave \'84', // Title of the panel displayed to the user
+		vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+		{ enableScripts: !0 } // Webview options. More on these later.
+	);
 
-		const viewPath = path.join(this.cntx.extensionPath, "whats-new", "view.html");
-		const viewResourcePath = panel.webview.asWebviewUri(viewPath);
-		const htmlContent = fs.readFileSync(viewPath, "utf-8");
-		panel.webview.html = htmlContent;
+	const viewPath = path.join(this.cntx.extensionPath, "whats-new", "view.html");
+	const viewResourcePath = panel.webview.asWebviewUri(viewPath);
+	const htmlContent = fs.readFileSync(viewPath, "utf-8");
+	panel.webview.html = htmlContent;
 }
 
 // this method is called when your extension is deactivated
@@ -164,7 +168,7 @@ function uninstall() {
 
 		vscode.window
 			.showInformationMessage("Neon Dreams disabled. VS code must reload for this change to take effect", { title: "Restart editor to complete" })
-			.then(function(msg) {
+			.then(function (msg) {
 				vscode.commands.executeCommand("workbench.action.reloadWindow");
 			});
 	} else {
